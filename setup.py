bf9721f5e036e6c2e7c95495d9896c846f6016c4
John Jacobsen <john@mail.npxdesigns.com>
2007-05-30 12:02:11 -0500

    Issue 1485: Use distutils to package up domapp-tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# setup.py
+# John Jacobsen, NPX Designs, Inc., jacobsen\@npxdesigns.com
+# Started: Wed May 30 16:39:14 2007
+
+from distutils.core import setup
+
+setup(name="domapp-tools",
+      version="WHATVERSION",
+      description="Tools for testing domapp and domapp.sbi",
+      author="John Jacobsen, NPX Designs, Inc. for UW-Madison",
+      author_email="john@mail.npxdesigns.com",
+      packages=["domapptools"],
+      )
